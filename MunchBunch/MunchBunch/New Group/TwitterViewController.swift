9419e9566dbb9830f660841cd24fd0bebbd9aac8
Kevin Nguyen <kevinlng@usc.edu>
2018-03-28 13:00:39 -0700

    Not including replies or retweets in timeline view

--- MunchBunch/MunchBunch/New Group/TwitterViewController.swift
+++ MunchBunch/MunchBunch/New Group/TwitterViewController.swift
@@ -14,8 +14,7 @@ class TwitterViewController: TWTRTimelineViewController {
 
     convenience init(handle: String) {
         let client = TWTRAPIClient()
-        let dataSource = TWTRUserTimelineDataSource(screenName: handle, apiClient: client)
-        
+        let dataSource = TWTRUserTimelineDataSource(screenName: handle, userID: nil, apiClient: client, maxTweetsPerRequest: 10, includeReplies: false, includeRetweets: false)
         self.init(dataSource: dataSource)
     }
     
@@ -24,7 +23,7 @@ class TwitterViewController: TWTRTimelineViewController {
     }
     
     required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("not implemented")
     }
     
     func formatHandle(handle: String) -> String {
